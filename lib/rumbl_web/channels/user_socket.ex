3dab68f2222c69d2e8245ba78e88a1879ab43858
big-meel <big.meel@zoho.com>
2021-10-31 15:08:53 -0500

    Add socket connections

---
+++ lib/rumbl_web/channels/user_socket.ex
@@ -0,0 +1,11 @@
+defmodule RumblWeb.UserSocket do
+  use Phoenix.Socket
+
+  channel "videos:*", RumblWeb.VideoChannel
+
+  def connect(_params, socket, _connect_info) do
+    { :ok, socket }
+  end
+
+  def id(_socket), do: nil
+end

38e7d4cb8b060e4e8f0d831f0efa549df1874835
big-meel <big.meel@zoho.com>
2021-10-30 22:29:19 -0500

    Push project

---
+++ lib/rumbl/accounts/user.ex
@@ -0,0 +1,42 @@
+defmodule Rumbl.Accounts.User do
+  # Bare user struct
+  # defstruct [:id, :name, :username] 
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  # Ecto schema for users
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username]) # converts raw map to a changeset
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params) # Calls changeset function above
+    |> cast(params, [:password]) # cast (think handle cast) is used to assert new state
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+      _ ->
+        changeset
+    end
+  end
+end

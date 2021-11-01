604ff7efb7ff5d3c87b99d2e88143274bf801f78
big-meel <big.meel@zoho.com>
2021-10-31 23:47:53 -0500

    Update annotation sockets

--- lib/rumbl_web/channels/video_channel.ex
+++ lib/rumbl_web/channels/video_channel.ex
@@ -4,4 +4,11 @@ defmodule RumblWeb.VideoChannel do
   def join("videos:" <> video_id, _params, socket) do
     {:ok, assign(socket, :video_id, String.to_integer(video_id ))}
   end
+
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push(socket, "ping", %{count: count})
+
+    {:noreply, assign(socket, :count, count + 1)}
+  end
 end

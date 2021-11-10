cff07e323a7ab8b17b95d1fb2807492cae5a7b98
big-meel <big.meel@zoho.com>
2021-11-10 06:12:20 -0500

    Update comments

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -8,7 +8,9 @@ defmodule InfoSys.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {InfoSys.Counter, 5}, # new counter worker { Module, initial_value} initial_value is received by start_link
+      # Supervisor.child_spec({InfoSys.Counter, 15}, id: :long), # new counter worker { Module, initial_value} initial_value is received by start_link
+      # Supervisor.child_spec({InfoSys.Counter, 5}, id: :short), # new counter worker { Module, initial_value} initial_value is received by start_link
+      # Supervisor.child_spec({InfoSys.Counter, 10}, id: :medium), # new counter worker { Module, initial_value} initial_value is received by start_link
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
